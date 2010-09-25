21d0b14e43eb87812d0883a0b6dd723ca747052f
Curtis J Schofield <github.com@robotarmyma.de>
2010-09-25 16:36:09 -0700

    final

--- button_set/main.lua
+++ button_set/main.lua
@@ -19,20 +19,25 @@ local bt = {}
 for x = 0,x_c,1 do
   for  y = 0,y_c,1 do
     local rect
-    local r = math.random( 0, 255 )
-    local g = math.random( 0, 255 )
-    local b = math.random( 0, 255 )
+    local r = math.random( 0, 200 )
+    local g = math.random( 0, 200 )
+    local b = math.random( 0, 200 )
 
     print("left" .. left)
     print("top" .. top)
     left = x*width
     top =  y*height
     rect = display.newRect(left,top,width,height) 
-    rect:setStrokeColor( r, g, b )
+    rect.strokeWidth = 4
+    rect:setStrokeColor( 0xFF, 0xFF, 0xFF )
     rect:setFillColor( r, g, b )
     function rect:tap(event) 
       print('-x:'..self.x.. '-y:'..self.y)
       print('-w:'..self.width.. '-h:'..self.height)
+    local r = math.random( 0, 200 )
+    local g = math.random( 0, 200 )
+    local b = math.random( 0, 200 )
+    self:setFillColor( r, g, b )
       print(self)
       print('---')
     end
