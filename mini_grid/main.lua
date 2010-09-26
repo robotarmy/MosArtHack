ee48373b639a2ff08fdb0e9230c8b6eb7012dbf4
Curtis J Schofield <github.com@robotarmyma.de>
2010-09-25 20:57:38 -0700

    OVERDRAW!

--- mini_grid/main.lua
+++ mini_grid/main.lua
@@ -43,14 +43,15 @@ function maxigrid:draw(mini,scale)
   dw = d_vcw / square_width
   dh = d_vch / square_width
   self.group = draw:tile(dw,dh,square_width,function(s,x,y)
-      local el = (x+y) % mini.num + 1
-      print(el)
-      print(mini.group[el].r)
-      s:setFillColor(mini.group[el].r,mini.group[el].g,mini.group[el].b)
+    local el = (x+y) % mini.num + 1
+    print(el)
+    print(mini.group[el].r)
+    s:setFillColor(mini.group[el].r,mini.group[el].g,mini.group[el].b)
   end)
 end
 
 minigrid = {}
+
 function minigrid:new(startx,starty,num,width)
   self.group = minigrid:drawTile(startx,starty,num,width) 
   self.num = num * num
@@ -59,23 +60,27 @@ function minigrid:new(startx,starty,num,width)
   assert(self.group.numChildren == self.num)
   return self
 end
+
 function minigrid:redraw()
   local parent = self.group.parent
   parent:insert(self.group)
 end
+
 function minigrid:drawTile(startx,starty,num,width) 
   local this = self
   return draw:tile(num-1,num-1,width,function (s)
-      radColor(s)
+    radColor(s)
+    s.strokeWidth = 2
+    s:setStrokeColor(0xff,0xff,0xff)
     function s:tap(event) 
-        print('-x:'..self.x.. '-y:'..self.y)
-        print('-w:'..self.width.. '-h:'..self.height)
-        radColor(self) 
-        print(self)
-        print('---')
-       maxigrid:draw(grid,50)
-       this:redraw ()
-      end
+      print('-x:'..self.x.. '-y:'..self.y)
+      print('-w:'..self.width.. '-h:'..self.height)
+      radColor(self) 
+      print(self)
+      print('---')
+      maxigrid:draw(grid,50)
+      this:redraw ()
+    end
     s:addEventListener( "tap", s )
   end,startx,starty)
 end
