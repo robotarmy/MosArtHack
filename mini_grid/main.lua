d77425207216dbdc63f0db3b9e4557d92be6c334
Curtis J Schofield <178963+robotarmy@users.noreply.github.com>
2010-09-25 23:29:50 -0700

    yay!
    teselated!
    
    props to shachaf !! for noticing a serious bug

--- mini_grid/main.lua
+++ mini_grid/main.lua
@@ -4,12 +4,23 @@
 --
 --
 
-local width,height = 100,100
+local width = 30
 
-function radColor(o)
-  local r = math.random( 0, 200 )
-  local g = math.random( 0, 200 )
-  local b = math.random( 0, 200 )
+local ram = {color = {0x193310,0x9EFA18,0xD4421E,0xA2C5A7,0x5C0903,0xFFFFFF}, sci = 1}
+function ram:current_color()
+  return self.color[self.sci]
+end
+
+function ram:next_color(o)
+  col = self:current_color()
+  print("before: " .. self.sci)
+
+  self.sci = (self.sci) % #self.color + 1
+  print("after: " .. self.sci)
+
+  local r = math.floor(col / 256 / 256)
+  local g = math.floor((col / 256) % 256)
+  local b = math.floor((col % 256) % 256)
   o.r = r
   o.g = g
   o.b = b
@@ -21,10 +32,10 @@ function draw:tile(xmax,ymax,width,callback,startx,starty)
   startx = startx or 0
   starty = starty or 0
   local group = display.newGroup()
-  for x = 0, xmax, 1 do
-    for y = 0, ymax, 1 do
+  for x = 1, xmax, 1 do
+    for y = 1, ymax, 1 do
       left = (x*width) + startx
-      top =  (y*width)  + starty
+      top =  (y*width) + starty
       local s = display.newRect(left,top,width,width) 
       group:insert(s)
       callback(s,x,y) 
@@ -39,13 +50,15 @@ function maxigrid:draw(mini,scale)
   scale = scale / 100
   local d_vcw =  display.viewableContentWidth
   local d_vch = display.viewableContentHeight
-  square_width = scale * mini.width
-  dw = d_vcw / square_width
-  dh = d_vch / square_width
+  square_width = math.floor(scale * mini.width)
+  dw = math.floor(d_vcw / square_width) +1 
+  dh = math.floor(d_vch / square_width) +1 -- draw offscreen
+  local el  = 0
   self.group = draw:tile(dw,dh,square_width,function(s,x,y)
-    local el = (x+y) % mini.num + 1
-    print(el)
-    print(mini.group[el].r)
+    el = (el  % mini.group.numChildren )+ 1
+    print('el ' .. el)
+    s.strokeWidth = 2
+    s:setStrokeColor(0xff,0xff,0xff)
     s:setFillColor(mini.group[el].r,mini.group[el].g,mini.group[el].b)
   end)
 end
@@ -56,7 +69,6 @@ function minigrid:new(startx,starty,num,width)
   self.group = minigrid:drawTile(startx,starty,num,width) 
   self.num = num * num
   self.width = width
-  print(self.group.numChildren)
   assert(self.group.numChildren == self.num)
   return self
 end
@@ -68,14 +80,14 @@ end
 
 function minigrid:drawTile(startx,starty,num,width) 
   local this = self
-  return draw:tile(num-1,num-1,width,function (s)
-    radColor(s)
+  return draw:tile(num,num,width,function (s)
+    ram:next_color(s)
     s.strokeWidth = 2
     s:setStrokeColor(0xff,0xff,0xff)
     function s:tap(event) 
       print('-x:'..self.x.. '-y:'..self.y)
       print('-w:'..self.width.. '-h:'..self.height)
-      radColor(self) 
+      ram:next_color(self) 
       print(self)
       print('---')
       maxigrid:draw(grid,50)
@@ -85,6 +97,6 @@ function minigrid:drawTile(startx,starty,num,width)
   end,startx,starty)
 end
 
-grid = minigrid:new(0,20,2,width)
+grid = minigrid:new(0,20,8,width)
 
 
